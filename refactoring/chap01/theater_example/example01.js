5465f82cde9ed9bb7dce752e257ea16a6a004199
2yunseong <dbsdltjd3701@naver.com>
2022-05-27 01:05:11 +0900

    refactor(리팩토링) : format 함수 이름 변경 - format -> usd - 단위 변환 기능도 수행하게 추가함.

--- refactoring/chap01/theater_example/example01.js
+++ refactoring/chap01/theater_example/example01.js
@@ -61,12 +61,12 @@ function volumeCreditsFor(perf){
     return result;
 }
 
-function format(number) {
+function usd(number) {
     return new Intl.NumberFormat("en-US", {
         style:"currency",
         currency: "USD",
         minimumFractionDigits: 2
-    }).format(number);
+    }).format(number/100);
 }
 
 function statement(invoice, plays){
@@ -79,10 +79,10 @@ let result = `청구내역: (고객명: ${invoice.customer})\n`;
 for(let perf of invoice.performances){
     volumeCredits = volumeCreditsFor(perf);
         // 청구 내역 출력
-result += `${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience}석)\n`;
+result += `${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience}석)\n`;
 totalAmount += amountFor(perf);
 }
-result += `총액: ${format(totalAmount/100)}\n`;
+result += `총액: ${usd(totalAmount)}\n`;
 result += `적립 포인트: ${volumeCredits}점\n`;
 return result;
 }
